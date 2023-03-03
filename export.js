b065ec111f8b02b953ba9fd8d0f5051ea6a1c91a
lorenasandoval88 <lorenasandoval88@gmail.com>
2023-03-02 21:58:35 -0500

    Update export.js

--- export.js
+++ export.js
@@ -3,8 +3,13 @@ let cdc={}
 cdc.data = async()=>{ 
     let url = `https://data.ny.gov/resource/5kgr-h5g5.json`
     console.log(url)
+    try{
     let d = await (await fetch(url)).json()
         return d
+    } catch (err){
+        console.log(`${url}`,err)
+        return undefined
+    } 
 }
 
 async function seer(){ 
