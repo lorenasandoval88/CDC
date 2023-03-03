ec09806b21d7294823c0c6c1e78f880ec295adb3
lorenasandoval88 <lorenasandoval88@gmail.com>
2023-03-02 23:41:10 -0500

    Update export.js

--- export.js
+++ export.js
@@ -1,7 +1,22 @@
 const hello = `hello at ${Date()}`
-let cdc={}
-cdc.data = async()=>{ 
+let openData={}
+
+openData.cdc = async()=>{ 
     let url = `https://data.ny.gov/resource/5kgr-h5g5.json`
+    console.log(url)
+    try{
+    let d = await (await fetch(url)).json()
+        return d
+    } catch (err){
+        console.log(`${url}`,err)
+        return undefined
+    } 
+}
+
+// ---------------------------------------------------------------------------
+openData.socrata = async()=>{ 
+    //Detailed Mortality 1999-2013 (D76)
+    let url = `https://wonder.cdc.gov/controller/datarequest/D76`
     console.log(url)
     try{
     let d = await (await fetch(url)).json()
@@ -17,6 +32,6 @@ cdc.data = async()=>{
 
 export { // these are the methods of epiVerseTracker
     hello,
-    cdc,
+    openData,
 
 }
